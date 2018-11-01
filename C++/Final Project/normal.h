e26d4a95b4efb808c616ce89ce9c0373f2cab641
KAIST-Lee-Festudy <kstlee10@kaist.ac.kr>
2018-11-02 01:41:20 +0900

    remove c++ directory

--- C++/Final Project/normal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-double normpdf(double x, double mu = 0., double sigma = 1.);
-double normcdf(double x, double mu = 0., double sigma = 1.);
-
-//enum d1_d2type { d1 = 1, d2 = -1 };
-//double d(double s, double k, double r, double q, double t, double sigma, d1_d2type d_j); // d1 ,d2 계산 함수 선언
